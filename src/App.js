481d3aada5515af4b944cbcb4ceca9936b41694c
AFutureWebDev <jenna.gullick2@gmail.com>
2019-05-29 19:45:16 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import FirstComponent from './firstComponent';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOn: false
+    }
+  }
+
+  toggle = () => {
+    this.setState({isOn: !this.state.isOn})
+  }
+
+  render() {
+    console.log("***THIS IS STATE***", this.state.isOn)
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World</h1>
+        <button onClick={this.render.toggle}>Submit</button>
       </header>
     </div>
-  );
+    );
+  }
 }
 
+
 export default App;
+
+// <FirstComponent/>
